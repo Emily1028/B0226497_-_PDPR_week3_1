06ef04462f49dfcc10cc379199ee1b143c53f930
Emily1028 <81636593+Emily1028@users.noreply.github.com>
2022-07-01 23:51:32 +0800

    加入專案檔案。

---
+++ Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace B0226497_段昱如_PDPR_week3_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label = new System.Windows.Forms.Label();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblresult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(57, 25);
+            this.label.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(278, 19);
+            this.label.TabIndex = 0;
+            this.label.Text = "B0226497_段昱如_PDPR_week3_1";
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(61, 108);
+            this.btnOK.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(125, 36);
+            this.btnOK.TabIndex = 1;
+            this.btnOK.Text = "確定";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(290, 108);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(125, 36);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "結束";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblresult
+            // 
+            this.lblresult.AutoSize = true;
+            this.lblresult.Location = new System.Drawing.Point(57, 201);
+            this.lblresult.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblresult.Name = "lblresult";
+            this.lblresult.Size = new System.Drawing.Size(68, 19);
+            this.lblresult.TabIndex = 3;
+            this.lblresult.Text = "lblresult";
+            this.lblresult.Click += new System.EventHandler(this.lblresult_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(833, 390);
+            this.Controls.Add(this.lblresult);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.label);
+            this.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblresult;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace B0226497_段昱如_PDPR_week3_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lblresult_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            string[,] SData = new string[5, 3];
+            int[,] SDegree = new int[5, 3];
+            double[,] Saver = new double[5, 1];
+            SData[0, 0] = "B0226497_1";
+            SData[0, 1] = "段昱如_1";
+            SData[0, 2] = "000001";
+            SData[1, 0] = "B0226497_2";
+            SData[1, 1] = "段昱如_2";
+            SData[1, 2] = "000002";
+            SData[2, 0] = "B0226497_3";
+            SData[2, 1] = "段昱如_3";
+            SData[2, 2] = "000003";
+            SData[3, 0] = "B0226497_4";
+            SData[3, 1] = "段昱如_4";
+            SData[3, 2] = "000004";
+            SData[4, 0] = "B0226497_5";
+            SData[4, 1] = "段昱如_5";
+            SData[4, 2] = "000005";
+            SDegree[0, 0] = 61;
+            SDegree[0, 1] = 72;
+            SDegree[0, 2] = 78;
+            SDegree[1, 0] = 97;
+            SDegree[1, 1] = 64;
+            SDegree[1, 2] = 79;
+            SDegree[2, 0] = 57;
+            SDegree[2, 1] = 68;
+            SDegree[2, 2] = 87;
+            SDegree[3, 0] = 76;
+            SDegree[3, 1] = 87;
+            SDegree[3, 2] = 78;
+            SDegree[4, 0] = 67;
+            SDegree[4, 1] = 68;
+            SDegree[4, 2] = 67;
+            Saver[0, 0] = (SDegree[0, 0] + SDegree[0, 1] + SDegree[0, 2]) / 3;
+            Saver[1, 0] = (SDegree[1, 0] + SDegree[1, 1] + SDegree[1, 2]) / 3;
+            Saver[2, 0] = (SDegree[2, 0] + SDegree[2, 1] + SDegree[2, 2]) / 3;
+            Saver[3, 0] = (SDegree[3, 0] + SDegree[3, 1] + SDegree[3, 2]) / 3;
+            Saver[4, 0] = (SDegree[4, 0] + SDegree[4, 1] + SDegree[4, 2]) / 3;
+            string str = "學生各項資料:";
+            str += $"\n學號:{SData[0, 0]},姓名:{SData[0, 1]},電話:{SData[0, 2]},國文:{SDegree[0, 0]},數學:{SDegree[0, 1]},英文:{SDegree[0, 2]},平均:{Saver[0, 0]}";
+            str += $"\n學號:{SData[1, 0]},姓名:{SData[1, 1]},電話:{SData[1, 2]},國文:{SDegree[1, 0]},數學:{SDegree[1, 1]},英文:{SDegree[1, 2]},平均:{Saver[1, 0]}";
+            str += $"\n學號:{SData[2, 0]},姓名:{SData[2, 1]},電話:{SData[2, 2]},國文:{SDegree[2, 0]},數學:{SDegree[2, 1]},英文:{SDegree[2, 2]},平均:{Saver[2, 0]}";
+            str += $"\n學號:{SData[3, 0]},姓名:{SData[3, 1]},電話:{SData[3, 2]},國文:{SDegree[3, 0]},數學:{SDegree[3, 1]},英文:{SDegree[3, 2]},平均:{Saver[3, 0]}";
+            str += $"\n學號:{SData[4, 0]},姓名:{SData[4, 1]},電話:{SData[4, 2]},國文:{SDegree[4, 0]},數學:{SDegree[4, 1]},英文:{SDegree[4, 2]},平均:{Saver[4, 0]}";
+            lblresult.Text = str;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
